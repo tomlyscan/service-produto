767aee866e37a5d45b8f4583eebac2e11ce16a05
thyago <thyago.oliveira>
2021-04-19 16:45:10 -0300

    init

--- src/main/java/com/example/serviceproduto/ServiceProdutoApplication.java
+++ src/main/java/com/example/serviceproduto/ServiceProdutoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceProdutoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args ) {
 		SpringApplication.run(ServiceProdutoApplication.class, args);
 	}
 
