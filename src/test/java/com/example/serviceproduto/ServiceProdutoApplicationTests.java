b9a72b03429b9f91ac66767ae6c403f655cf4119
thyago <thyago.oliveira>
2021-04-19 16:43:33 -0300

    init

---
+++ src/test/java/com/example/serviceproduto/ServiceProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
